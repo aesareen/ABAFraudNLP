fdfed449bdf02703fa999bafc31ca325a24671e3
aesareen <arnavsareen@gmail.com>
2025-11-13 23:43:48 -0500

    Add Supabase upload script
    Added a script to upload JSON articles to Supabase, including error handling and progress display. Introduced a .env.example file for environment variables and updated dependencies to include python-dotenv.

---
+++ scripts/upload_to_supabase.py
@@ -0,0 +1,37 @@
+from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+import glob
+import json
+from datetime import datetime, timezone
+from rich import print
+from postgrest.exceptions import APIError
+from rich.progress import Progress
+
+load_dotenv(dotenv_path="config/.env")
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+
+CLIENT: Client = create_client(url, key)
+
+def upload_to_supabase(json_content: dict):
+    json_dict = json_content[0]
+    json_dict['created_at'] = datetime.now(timezone.utc).isoformat()
+    response = CLIENT.table("articles").insert(json_dict).execute()
+    return response.data
+
+if __name__ == "__main__":
+    # Load in all the JSON files in our data folder
+    json_files = glob.glob("data/scraped_json_results/*.json")
+    with Progress() as progress:
+        for file in progress.track(json_files, description="Uploading articles"):
+            with open(file, "r", encoding="utf-8") as f:
+                json_content = json.load(f)
+            try:
+                response = upload_to_supabase(json_content)
+                print(f"[green]Successfully uploaded {file} to Supabase[/green]")
+            except APIError as e:
+                print(f"[red]Failed to upload {file} to Supabase[/red]")
+                print(f"Error: {e}")
+                continue
